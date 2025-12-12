1497f495f81e2ba3627423492a23a7271ddfe89f
Jonas <jonas@jonaskoperdraat.nl>
2025-12-12 23:27:52 +0100

    Working tofu config

--- platform/external/variables.tf
+++ platform/external/variables.tf
@@ -1,3 +1,4 @@
+
 variable "ntfy" {
   type = object({
     url   = string
--- platform/external/versions.tf
+++ platform/external/versions.tf
@@ -1,9 +1,9 @@
 terraform {
   required_version = "~> 1.7"
 
-  backend "remote" {
+  cloud {
     hostname     = "app.terraform.io"
-    organization = "yona-works"
+    organization = "jnskt"
 
     workspaces {
       name = "homelab-platform-external"
@@ -24,6 +24,4 @@ terraform {
 }
 
 provider "kubernetes" {
-  # Use KUBE_CONFIG_PATH environment variables
-  # Or in cluster service account
 }
